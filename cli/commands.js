1a4792b967ffd066ad4e8bf696e4da0c3ec4245a
Sani Elfishawy <elfishawy.sani@gmail.com>
2024-05-09 12:25:36 -0700

    status

--- cli/commands.js
+++ cli/commands.js
@@ -6,60 +6,54 @@ import * as MApi from '../api/Esp32MotorApi.js'
 
 function runCli(){
     let c = program.command('getFreq')
-    c.action( async (options) => {await handleGetFreq(options)})
+    c.action( async (options) => {
+        await printResponse(await MApi.getFreqHz(options))
+    })
 
     c = program.command('getAmp')
-    c.action( async (options) => {await handleGetAmp(options)})
+    c.action( async (options) => { 
+        await printResponse(await MApi.getAmplitudeFract(options))
+    })
 
     c = program.command('setFreq <freq>')
-    c.action( async (freq, options) => {await handleSetFreq(freq, options)})
+    c.action( async (freq, options) => { 
+        await printResponse(await MApi.setFreqHz(freq, options))
+    })
 
     c = program.command('setAmp <amp>')
-    c.action( async (amp, options) => {await handleSetAmp(amp, options)}) 
+    c.action( async (amp, options) => { 
+        await printResponse(await MApi.setAmplitudeFract(amp, options))
+    })
 
     c = program.command('setActive')
-    c.action( async (options) => {await handleSetActive(options)})
+    c.action( async (options) => { 
+        await printResponse(await MApi.setActive(options))
+    })
 
     c = program.command('setFloat')
-    c.action( async (options) => {await handleSetFloat(options)})
+    c.action( async (options) => { 
+        await printResponse(await MApi.setFloat(options))
+    })
     
     c = program.command('getIsActive')
-    c.action( async (options) => {await handleGetIsActive(options)})
+    c.action( async (options) => { 
+        await printResponse(await MApi.getIsActive(options))
+    })
+
+    c = program.command('status')
+    c.action( async (options) => { 
+        await printResponse(await MApi.getStatus())
+    })
 
     program.parse(process.argv)
 }
 
-async function handleGetFreq(options){
-    printResponse(await MApi.getFreqHz())
-}
-
-async function handleGetAmp(options){
-    printResponse(await MApi.getAmplitudeFract())
-}
-
-async function handleSetFreq(freq, options){
-    printResponse(await MApi.setFreqHz(freq))
-}
-
-async function handleSetAmp(amp, options){
-    printResponse(await MApi.setAmplitudeFract(amp))
-}
-
-async function handleSetActive(options){
-    printResponse(await MApi.setActive())
-}
-
-async function handleSetFloat(options){
-    printResponse(await MApi.setFloat())
-}   
-
-async function handleGetIsActive(options){
-    printResponse(await MApi.getIsActive())
-}
-
 async function printResponse(r){
     const j = await r.json()
-    return console.log(util.inspect(j, false, null, true))
+    const formattedObject = JSON.parse(JSON.stringify(j, (key, value) => {
+        return typeof value === 'number' ? Number(value.toFixed(3)) : value;
+    }));
+    console.log(util.inspect(formattedObject, false, null, true))
 }
 
 runCli()
